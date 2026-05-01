619c7b9c121fef1a572cd32f598649be9f0103c9
Lekan <lekzyadebox@gmail.com>
2026-05-01 10:24:47 +0100

    git commit -m "Refactor landing page structure and imports"

--- EduChain/src/main.jsx
+++ EduChain/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LandingPage from '../src/pages/LandingPage'
+import LandingPage from '../src/pages/landingPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
